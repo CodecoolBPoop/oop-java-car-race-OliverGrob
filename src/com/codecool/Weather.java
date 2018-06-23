44b27a4ecea2eeadd149e13733c13c17d6b19cb4
Grob Oliver <groboliver1117@gmail.com>
2018-06-23 20:00:55 +0200

    Weather class created with method signatures

---
+++ src/com/codecool/Weather.java
@@ -0,0 +1,20 @@
+package com.codecool;
+
+public class Weather {
+
+    /**
+     * 30% chance of rain.
+     */
+    public static void setRaining() {
+
+    }
+
+    /**
+     * Returns whether it is raining currently or not.
+     *
+     * @return : True if it's raining, false otherwise
+     */
+    public static boolean isRaining() {
+        return false;
+    }
+}
