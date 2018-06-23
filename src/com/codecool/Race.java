b77d671ce474db2a1d021521f52f1a135c86d574
Grob Oliver <groboliver1117@gmail.com>
2018-06-23 19:55:35 +0200

    Created methods in Race

--- src/com/codecool/Race.java
+++ src/com/codecool/Race.java
@@ -2,6 +2,38 @@ package com.codecool;
 
 public class Race {
 
+    /**
+     * Creates 10 cars, 10 trucks and 10 motorcycles.
+     */
+    public static void createVehicles() {
+
+    }
+
+    /**
+     * Simulate the race by
+     *    - calling moveForAnHour() on every vehicle 50 times
+     *    - setting whether its raining
+     */
+    public static void simulateRace() {
+
+    }
+
+    /**
+     * Prints each vehicle's name, distance traveled ant type.
+     */
+    public static void printRaceResults() {
+
+    }
+
+    /**
+     * Decides if there is a broken truck on the track
+     *
+     * @return : True if there is a broken truck, false if there is not
+     */
+    public static boolean isThereABrokenTruck() {
+        return false;
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
