b069e5f78fd81ea8e2939c14ad7d698df43ede2b
Grob Oliver <groboliver1117@gmail.com>
2018-07-02 20:53:38 +0200

    vehicle is abstract class not an interface anymore

--- src/com/codecool/Vehicle.java
+++ src/com/codecool/Vehicle.java
@@ -1,6 +1,44 @@
 package com.codecool;
 
-public interface Vehicle {
+public abstract class Vehicle {
+
+    private int normalSpeed;
+    private String name;
+    private int distanceTraveled = 0;
+    private String type;
+
+
+    public int getNormalSpeed() {
+        return this.normalSpeed;
+    }
+
+    public void setNormalSpeed(int normalSpeed) {
+        this.normalSpeed = normalSpeed;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDistanceTraveled() {
+        return this.distanceTraveled;
+    }
+
+    public void setDistanceTraveled(int distanceTraveled) {
+        this.distanceTraveled = distanceTraveled;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     /**
      * The vehicle travels for an hour. It increases the distance traveled. Call this from the main class only!
@@ -8,5 +46,6 @@ public interface Vehicle {
      *
      * @param race : The race the vehicle is participate
      */
-    public void moveForAnHour(Race race);
+    public abstract void moveForAnHour(Race race);
+
 }
